53c4dd2e413f4eaa16a0289797b0860c8579f472
stepheniota <iota@usc.edu>
2022-01-26 16:59:29 -0800

    change label format

--- hw1-naive-bayes/reviews_data.py
+++ hw1-naive-bayes/reviews_data.py
@@ -5,9 +5,9 @@ import numpy as np
 class ReviewsData:
     def __init__(self, data_path, is_training=True):
         self.data_path = Path(data_path)
-        self.is_training = is_training
         self.data = []
-        self.labels = []
+        self.labels = [] if is_training else None
+        self.paths = [] if is_training else None
 
     def read_txt(self):
         paths = self.data_path.glob('**/*.txt')
@@ -18,9 +18,11 @@ class ReviewsData:
                 self.data.append(np.loadtxt(p, dtype=str))
             except Exception as e:
                 print(f'{p} failed due to exception {e}')
-            positive = 'positive' in p.parts[-4]
-            truthful = 'truthful' in p.parts[-3]
-            self.labels.append(np.array([positive, truthful]))
+            if self.labels and self.paths:
+                positive = 'positive' if 'positive' in p.parts[-4] else 'negative'
+                truthful = 'truthful' if 'truthful' in p.parts[-3] else 'deceptive'
+                self.labels.append([positive, truthful])
+                self.paths.append(p)
 
     def preprocess(self):
         pass
