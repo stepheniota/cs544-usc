63bd8390fd8fbf870d205df72294d65c94992a99
stepheniota <iota@usc.edu>
2022-01-27 12:33:12 -0800

    remove stop words

--- hw1-naive-bayes/reviews_data.py
+++ hw1-naive-bayes/reviews_data.py
@@ -1,40 +1,79 @@
 import re
+import heapq
 from pathlib import Path
+from collections import defaultdict
 
 import numpy as np
 
+from utils import Params
+
+
 class ReviewsData:
-    def __init__(self, data_path, is_training=True):
+    def __init__(self, data_path, is_training=True, remove_stopwords=True):
         self.data_path = Path(data_path)
         self.X = []
         self.y = [] if is_training else None
         self.paths = []
+        self.classes = Params().classes
+        self.stopwords = Params().stopwords
+        self.remove_stopwords = remove_stopwords
 
     def read_txt(self):
         paths = self.data_path.glob('**/*.txt')
         for p in paths:
             if p.parts[-1] == 'README.txt':
                 continue
-            try:
-                self.X.append(np.loadtxt(p, dtype=str))
-            except Exception as e:
-                print(f'{p} failed due to exception {e}')
+            with open(p, 'r') as f:
+                self.X.append(f.readlines())
             self.paths.append(p)
             if self.y is not None:
                 truthful = 'truthful' if 'truthful' in p.parts[-3] else 'deceptive'
                 positive = 'positive' if 'positive' in p.parts[-4] else 'negative'
                 self.y.append(truthful + '_' + positive)
 
-    def preprocess(self):
+    def preprocess(self, n_largest=1000):
         self._normalize()
+        self._tokenize()
+        self._feature_selection(n_largest)
+    
+    def _tokenize(self):
+        for i, doc in enumerate(self.X):
+            if self.remove_stopwords:
+                doc = ' '.join([word for word in doc.split() if word not in self.stopwords])
+            doc = doc.split(' ')
+            self.X[i] = doc
 
 
     def _normalize(self):
-        for doc in self.X:
-            for i, word in enumerate(doc):
-                word = word.lower()
-                word = re.sub(r'\d+','', word)
-                word = re.sub(r'[^\w\s]', '', word)
-                word = re.sub(r'[!@#$.,]', '', word)
-                word = word.strip()
-                doc[i] = word
+        for i, doc in enumerate(self.X):
+            doc = doc[0].lower()
+            doc = re.sub(r'\d+','', doc)
+            doc = re.sub(r'[^\w\s]', '', doc)
+            doc = re.sub(r'[!@#$.,]', '', doc)
+            doc = doc.strip()
+            self.X[i] = doc
+
+    def _feature_selection(self, n_largest):
+        word_existence_per_doc = {cls:defaultdict(lambda:0) for cls in self.classes}
+
+        for cls in self.classes:
+            for doc in self.X:
+                cur_words = set(doc)
+                for word in cur_words:
+                    word_existence_per_doc[cls][word] += 1
+
+        top_words = {cls:None for cls in self.classes}
+        for cls in self.classes:
+            top = heapq.nlargest(n_largest, 
+                                 word_existence_per_doc[cls], 
+                                 word_existence_per_doc[cls].get)
+            top_words[cls] = top
+        for cls in self.classes:
+            for i, (doc, label) in enumerate(zip(self.X, self.y)):
+                if label == cls:
+                    new_doc = []
+                    for word in doc:
+                        if word in top_words[cls]:
+                            new_doc.append(word)
+                    self.X[i] = new_doc
+        
