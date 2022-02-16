3c6cc249913d4ba6a68110a6ee8d6e214d14fb71
stepheniota <iota@usc.edu>
2022-02-16 11:44:22 -0800

    implement iterator and data shuffling

--- hw2-hmm/pos_data.py
+++ hw2-hmm/pos_data.py
@@ -1,15 +1,21 @@
 """Dataclass."""
+import random
+
 from pathlib import Path
-from typing import Union
+from typing import Union, List, Tuple
+
+from numpy import true_divide
 
 class POSData:
     """Dataclass for tagged parts-of-speech data."""
     def __init__(self,
                  data: Union[str, Path],
                  train: bool,
-                 read: bool = True) -> None:
+                 read: bool = True,
+                 shuffle: bool = True) -> None:
         self.data = data
         self.train = train
+        self.shuffle = shuffle
         self.X, self.y = [], []
         if read:
             self.read_txt()
@@ -32,6 +38,32 @@ class POSData:
                 else:
                     self.X.append(line.split())
 
+    def _shuffle(self,) -> None:
+        tmp_list = list(zip(self.X, self.y))
+        random.shuffle(tmp_list)
+        self.X, self.y = zip(*tmp_list)  # * -> unzip list
+    
+    def __iter__(self):
+        if self.shuffle:
+            self._shuffle()
+        for x, y in zip(self.X, self.y):
+            yield x, y
+
+    def __getitem__(self, idx):
+        return self.X[idx], self.y[idx]
+
+    def __len__(self,):
+        return len(self.X)
+
+
 if __name__ == "__main__":
-    data = POSData("hmm-training-data/it_isdt_train_tagged.txt", True, True)
-    print(data.X)
+    data = POSData(
+            data="hmm-training-data/it_isdt_train_tagged.txt", 
+            train=True, 
+            shuffle=True, 
+            read=True
+        )
+    for x, y in data:
+        print(x, y)
+        break
+    
