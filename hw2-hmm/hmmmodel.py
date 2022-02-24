e1f9a66589718872b06490cd9e68a39cac69e423
stepheniota <iota@usc.edu>
2022-02-23 23:13:47 -0800

    rewrite viterbi using np array

--- hw2-hmm/hmmmodel.py
+++ hw2-hmm/hmmmodel.py
@@ -1,6 +1,8 @@
 """Hidden Markov model class."""
 import json
 
+import numpy as np
+
 from utils import *
 
 class HMM:
@@ -13,7 +15,7 @@ class HMM:
         self.trans = None  # assume categorical dist.
         self.emiss = None  # assume categorical dist.
 
-    def fit(self, X, y, n_open=4):
+    def fit(self, X, y, n_open=None):
         """Estimate HMM parameters."""
         self._init_params(X, y)
         self._fit_params(X, y)
@@ -22,6 +24,10 @@ class HMM:
 
     def _open_states(self, n_open):
         """Determine open states for unseen obs."""
+        if n_open is None:
+            self.open_states = {st: 1 for st in self.states}
+            return
+
         vocab = {st: len(obs) for st, obs in self.emiss.items()}
         vocab_list = sorted(list(vocab), key=vocab.get, reverse=True)
         vocab_list = vocab_list[:n_open]
@@ -30,10 +36,11 @@ class HMM:
     def _init_params(self, X, y):
         """Initialize state space, obs space and priors."""
         self.states = set(st for state_seq in y for st in state_seq)
+        self.states = list(self.states)
         self.obs = set(o for obs_seq in X for o in obs_seq)
         # NOTE: plus-one smoothing for all possible combinations.
-        self.init_probs = {st: 1. for st in self.states}
-        self.trans = {prev_st: {st: 1. for st in self.states} for prev_st in self.states}
+        self.init_probs = {st: 1 for st in self.states}
+        self.trans = {prev_st: {st: 10 for st in self.states} for prev_st in self.states}
         self.emiss = {st: {} for st in self.states}
 
     def _fit_params(self, X, y):
@@ -68,74 +75,65 @@ class HMM:
     def viterbi(self, obs_seq, likelihood):
         """Viterbi decoding algorithm."""
         T = len(obs_seq)
-        Pr = {t: {st: 0. for st in self.states} for t in range(T)}
-        V = {t: {st: None for st in self.states} for t in range(T)}
+        v_prob = np.zeros((T, len(self.states)))
+        v_ptrs = {t: {} for t in range(T)}
 
-        # Base case fwd
+        # base case
         init_obs = obs_seq[0]
-        for st in self.states:
+        for i, st in enumerate(self.states):
+            v_ptrs[0][st] = None
             if init_obs in self.emiss[st]:
-                Pr[0][st] = likelihood(self.init_probs[st], self.emiss[st][init_obs])
+                v_prob[0][i] = likelihood(self.init_probs[st], self.emiss[st][init_obs])
             elif init_obs not in self.obs and st in self.open_states:
-                # obs unseen during training, consider only open states.
-                Pr[0][st] = likelihood(self.init_probs[st])
+                v_prob[0][i] = likelihood(self.init_probs[st])
             else:
-                Pr[0][st] = 0.
+                v_prob[0][i] = 0
 
-        self._forward(Pr, V, obs_seq, likelihood)
+        # forward pass
+        for t, obs in enumerate(obs_seq[1:], start=1):
+            for i, st in enumerate(self.states):
+                if obs in self.emiss[st]:
+                    emiss = self.emiss[st][obs]
+                    best_pr, best_st = self._likelihood(v_prob, st, i,
+                                                        t, likelihood, emiss)
+                elif obs not in self.obs and st in self.open_states:
+                    best_pr, best_st = self._likelihood(v_prob, st, i,
+                                                        t, likelihood)
+                else:
+                    best_pr, best_st = 0, None
 
-        best_state = max(Pr[T-1], key=Pr[T-1].get)
-        best_path = self._backward(V, best_state, T)
+                v_prob[t][i] = best_pr
+                v_ptrs[t][st] = best_st
+
+        # backward pass
+        #return v_prob, v_ptrs
+        last_st_idx = np.argmax(v_prob[-1])
+        last_st = self.states[last_st_idx]
+        best_path = self._viterbi_bkw(v_ptrs, last_st, T)
 
         return best_path
 
-    def _forward(self, Pr, V, obs_seq, likelihood):
-        """Fwd pass of the viterbi decoding algorithm."""
-        for t, obs in enumerate(obs_seq[1:], start=1):
-            for st in self.states:
-                if obs in self.emiss[st]:
-                    emiss_prob = self.emiss[st][obs]
-                    prev_max_st = self._compute_max_state(Pr, t, st, emiss_prob,
-                                                          likelihood)
-                elif obs not in self.obs and st in self.open_states:
-                    # obs unseen during training, consider only open states.
-                    emiss_prob = self.open_states[st]
-                    prev_max_st = self._compute_max_state(Pr, t, st, emiss_prob,
-                                                          likelihood)
-                else:
-                    Pr[t][st] = 0.
-                if 0 != Pr[t][st]:
-                    V[t][st] = prev_max_st
+    def _viterbi_bkw(self, v_ptrs, last_st, T):
+        best_path = [last_st]
+        for t in range(T-1, 0, -1):
+            last_st = v_ptrs[t][last_st]
+            best_path.append(last_st)
 
-    def _backward(self, V, best_state, T):
-        """Backtracking."""
-        out_path = [best_state]
-        try:
-            for t in range(T-1, 0, -1):
-                best_state = V[t][best_state]
-                out_path.insert(0, best_state)
-            return out_path
-        except KeyError:
-            print(f"KeyError, t={t}.")
-            while t > 0:
-                out_path.append(None)
-                t -= 1
-                assert len(out_path) == T
-                return out_path
+        return best_path[::-1]
 
-    def _compute_max_state(self, Pr, t, state, emiss_prob, likelihood):
-        """Argmax."""
-        max_state = None
-        for prev_st in self.states:
-            prob_so_far = Pr[t-1][prev_st]
-            trans_prob = self.trans[prev_st][state]
+    def _likelihood(self, v_prob, st, i_st, t, likelihood, emiss=None):
+        best_st, best_pr = None, np.float("-inf")
+        for i_prev_st, prev_st in enumerate(self.states):
+            trans = self.trans[prev_st][st]
+            probsofar = v_prob[t-1][i_prev_st]
+            if emiss is not None:
+                cur_pr = likelihood(trans, probsofar, emiss)
+            else:
+                cur_pr = likelihood(trans, probsofar)
+            if cur_pr > best_pr:
+                best_pr, best_st = cur_pr, prev_st
 
-            cur_prob = likelihood(prob_so_far, trans_prob, emiss_prob)
-            if cur_prob > Pr[t][state]:
-                Pr[t][state] = cur_prob
-                max_state = prev_st
-
-        return max_state
+        return best_pr, best_st
 
     def save_params(self, file_name="hmmmodel.txt"):
         """Write model params to human-interpretable, json-format txt file."""
@@ -155,6 +153,6 @@ class HMM:
             params = json.load(f)
 
         for attr_name, val in params.items():
-            if isinstance(val, list):
+            if isinstance(val, list) and attr_name != "states":
                 val = set(val)
             setattr(self, attr_name, val)
